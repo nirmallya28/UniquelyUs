129567bec453dbc9b940ffa10af48167661b4784
nirmallya28 <157309613+nirmallya28@users.noreply.github.com>
2025-07-14 00:17:30 +0530

    Update server.js

--- social-media-app/server.js
+++ social-media-app/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require("cors");
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'https://autism-platform-frontend.vercel.app',
+  credentials: true,
+}));
 const path = require("path");
 
 const app = express();
