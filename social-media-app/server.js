0642ef6fa7181e889cf9d5a344414ffc8c9b2cf3
nirmallya28 <157309613+nirmallya28@users.noreply.github.com>
2025-07-14 00:34:42 +0530

    Update server.js

--- social-media-app/server.js
+++ social-media-app/server.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require('cors');
 
-app.use(cors({
-  origin: 'https://autism-platform-frontend.vercel.app',
-  credentials: true,
-}));
+
 const path = require("path");
 
 const app = express();
@@ -14,7 +11,10 @@ dotenv.config();
 
 // Middleware
 app.use(express.json({ limit: "20mb" })); // Accept large base64 drawings
-app.use(cors());
+app.use(cors({
+  origin: 'https://autism-platform-frontend.vercel.app',
+  credentials: true,
+}));
 
 
 // MongoDB Connection
